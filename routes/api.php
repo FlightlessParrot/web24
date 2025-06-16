4a5a94fcd33d9d88f827958ceaaf547eaa047883
Konrad Strauss <shrimpinweb@gmail.com>
2025-06-16 17:01:54 +0200

    routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\AuthenticatedController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\CompanyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,14 @@ Route::post('/login', [AuthenticatedController::class, 'store'])
 Route::middleware(['auth:sanctum'])->post('/logout', [AuthenticatedController::class, 'destroy'])
         ->name('api.logout');
 
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::resource('companies', CompanyController::class)
+        ->only(['index', 'show', 'store', 'update', 'destroy'])
+        ->names([
+            'index' => 'api.companies.index',
+            'show' => 'api.companies.show',
+            'store' => 'api.companies.store',
+            'update' => 'api.companies.update',
+            'destroy' => 'api.companies.destroy',
+        ]);
+});
